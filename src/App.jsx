dd5eee53c562e5aebe683a88475dff1a30751af2
David Andersson <david.andersson@zocom.se>
2023-10-16 13:33:23 +0200

    Initial files

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+// import { useState } from 'react'
+import './App.css'
+
+
+function App() {
+	// const [count, setCount] = useState(0)
+
+	return (
+		<main>
+			<h1> Charts </h1>
+		</main>
+	)
+}
+
+export default App
