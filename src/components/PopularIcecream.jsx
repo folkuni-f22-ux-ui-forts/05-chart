c60dddeb8fbaeb357cd20cd0fcaed2e65f18169a
David Andersson <david.andersson@zocom.se>
2023-10-16 15:06:59 +0200

    Bar chart

--- src/components/PopularIcecream.jsx
+++ src/components/PopularIcecream.jsx
@@ -1,11 +1,14 @@
-import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
-ChartJS.register(ArcElement, Tooltip, Legend);
-import { Pie } from 'react-chartjs-2'
+import { Chart as ChartJS, ArcElement, Tooltip, Legend, CategoryScale, LinearScale, BarElement } from 'chart.js';
+ChartJS.register(ArcElement, Tooltip, Legend, CategoryScale, LinearScale, BarElement);
+import { Pie, Bar } from 'react-chartjs-2'
 
+// Uppgift: lägg till ett stapeldiagram
+// För stapeldiagram behöver man: BarElement, LinearScale
 
-import { getConfig } from '../data/getIcecream'
+import { getPieConfig, getBarConfig } from '../data/getIcecream'
 
-const pieConfig = getConfig()
+const pieConfig = getPieConfig()
+const barConfig = getBarConfig()
 
 
 const PopularIcecream = () => {
@@ -13,6 +16,7 @@ const PopularIcecream = () => {
 	return (
 		<section>
 			<h2> Most popular icecream types! </h2>
+			<Bar data={barConfig} />
 			<Pie data={pieConfig} />
 		</section>
 	)
--- src/data/getIcecream.js
+++ src/data/getIcecream.js
@@ -1,15 +1,33 @@
 import data from './icecream.json'
 
+const colors = ['#279696', '#bae6ac', '#62a87c', '#c80000', '#6338e2']
 
-export function getConfig() {
-	console.log('Icecream data: ', data)
+export function getPieConfig() {
+	// console.log('Icecream data: ', data)
 	return {
 		labels: data.map(obj => obj.type),
 		datasets: [{
 			label: 'Popular icecreams',
 			data: data.map(obj => obj.popularity),
-			backgroundColor: ['#279696', '#bae6ac', '#62a87c', '#c80000', '#6338e2'],
-			// borderColors:
+			backgroundColor: colors,
+			// borderColor:
+		}]
+	}
+}
+
+export function getBarConfig() {
+	const sortedData = [ ...data ].sort((a, b) => {
+		if( a.popularity > b.popularity ) return -1
+		else if (a.popularity < b.popularity ) return 1
+		else return 0
+	})
+	return {
+		labels: sortedData.map(obj => obj.type),
+		datasets: [{
+			label: 'Popular icecreams',
+			data: sortedData.map(obj => obj.popularity),
+			backgroundColor: colors,
+			borderColor: ['#000000']
 		}]
 	}
 }
